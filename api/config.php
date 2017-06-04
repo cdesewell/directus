fd86ee1cbd0ae514cd1b0768f54052cd0a032174
Chris <chris.de.sewell@gmail.com>
2017-06-04 12:12:14 +0100

    use db name

--- api/config.php
+++ api/config.php
@@ -17,7 +17,7 @@ define('DIRECTUS_ENV', 'development');
 define('DB_TYPE', 'mysql');
 define('DB_HOST', getenv('DB_URL'));
 define('DB_PORT', 3306);
-define('DB_NAME', 'cms');
+define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_PREFIX', '');
